03e3db90efc786ff1fbe806ef5795d781567186c
Administrator <Administrator@10.134.112.141>
2023-05-09 15:13:11 +0700

    HuyTV28 commit

--- src/validation/ValidationGiaoVien.java
+++ src/validation/ValidationGiaoVien.java
@@ -3,6 +3,8 @@ package validation;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
+import function.FunctionGiaoVien;
+
 public class ValidationGiaoVien {
 
 	public ValidationGiaoVien() {
@@ -27,12 +29,14 @@ public class ValidationGiaoVien {
 		do {
 			System.out.println(message);
 			String idMonHoc = input.nextLine();
-			Pattern pattern = Pattern.compile("^MH[0-9]{3,3}$");
-			if (pattern.matcher(idMonHoc).find()) {
-
-				return idMonHoc;
+			FunctionGiaoVien check = new FunctionGiaoVien();
+			if (check.checkIDMonHoc(idMonHoc)) {
+				Pattern pattern = Pattern.compile("^MH[0-9]{3,3}$");
+				if (pattern.matcher(idMonHoc).find()) {
+					return idMonHoc;
+				}
+				System.out.println("ban nhap sai ma ID Mon HOc, hay nhap lai theo format (MH***)");
 			}
-			System.out.println("ban nhap sai ma ID Mon HOc, hay nhap lai theo format (MH***)");
 		} while (true);
 	}
 
@@ -53,14 +57,17 @@ public class ValidationGiaoVien {
 		do {
 			System.out.println(message);
 			String userName = input.nextLine();
-			Pattern pattern = Pattern.compile("^[a-zA-Z_0-9]*$");
-			if (pattern.matcher(userName).find()) {
-
-				return userName;
+			FunctionGiaoVien check = new FunctionGiaoVien();
+			if (check.checkUserName(userName)) {
+				Pattern pattern = Pattern.compile("^[a-zA-Z_0-9]*$");
+				if (pattern.matcher(userName).find()) {
+					return userName;
+				}
+				System.out.println("ban nhap sai định dang của username, username k chứa ký tự đặc biệt, hay nhap lai");
 			}
-			System.out.println("ban nhap sai định dang của username, username k chứa ký tự đặc biệt, hay nhap lai");
 		} while (true);
 	}
+
 	public int inputSoNamKinhNghiem(String message) {
 		do {
 			try {
@@ -72,7 +79,8 @@ public class ValidationGiaoVien {
 				System.out.println("hay nhap lai diem danh gia bang chu so");
 			}
 		} while (true);
-	} 
+	}
+
 	public String inputDiaChi(String message) {
 		do {
 			System.out.println(message);
@@ -84,7 +92,7 @@ public class ValidationGiaoVien {
 			System.out.println("ban nhap sai, hay nhap lai thon tin dia chi it nhat 2 ky tu!");
 		} while (true);
 	}
-	
+
 	public String inputSDT(String message) {
 		do {
 			System.out.println(message);
@@ -97,7 +105,7 @@ public class ValidationGiaoVien {
 			System.out.println("ban nhap sai ma so giao vien, hay nhap lai theo format 10 chu so");
 		} while (true);
 	}
-	
+
 	public String inputEmail(String message) {
 		do {
 			System.out.println(message);
@@ -108,12 +116,11 @@ public class ValidationGiaoVien {
 				return email;
 			}
 			System.out.println("ban nhap sai định dang của email, email cần phải có định dạng "
-					+ "- Bắt đầu bằng chữ cái.\r\n"
-					+ "- Chỉ chứa chữ cái, chữ số và dấu gạch ngang (-).\r\n"
+					+ "- Bắt đầu bằng chữ cái.\r\n" + "- Chỉ chứa chữ cái, chữ số và dấu gạch ngang (-).\r\n"
 					+ "- Chứa một ký tự @, sau @ là tên miền.\r\n"
 					+ "- Tên miền có thể là domain.xxx.yyy hoặc domain.xxx. Trong đó xxx và yyy là các chữ cái và có độ dài từ 2 trở lên., "
 					+ "Hay nhap lai");
 		} while (true);
 	}
-	
+
 }
