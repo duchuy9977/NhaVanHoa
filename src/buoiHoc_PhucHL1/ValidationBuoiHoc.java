6e3439134ff9dbd27b16e98bf3666412ba96052f
GiaNguyen-PC <GiaNguyen-PC@DESKTOP-TMVNIPS>
2023-05-10 19:57:48 +0700

    CapNhat

---
+++ src/buoiHoc_PhucHL1/ValidationBuoiHoc.java
@@ -0,0 +1,55 @@
+package buoiHoc_PhucHL1;
+
+public class ValidationBuoiHoc {
+
+	public String inputString(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String inputIdBuoiHoc(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getIdBuoiHoc() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getIdLop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getIdGiaoVien() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getThu() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getTgianHoc() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getPhongHoc() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getNgayHoc() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getStatus() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
