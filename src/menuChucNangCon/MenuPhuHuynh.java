2ed8bc62984beda7e505d2840a70732aa5907014
Admin <Admin@10.134.113.245>
2023-05-12 13:35:21 +0700

    PhatNT26 - update chức năng con

--- src/menuChucNangCon/MenuPhuHuynh.java
+++ src/menuChucNangCon/MenuPhuHuynh.java
@@ -22,36 +22,43 @@ public class MenuPhuHuynh {
 				System.out.println("3. Xóa thông tin Phụ huynh");
 				System.out.println("4. Liệt kê thông tin học phí của các phụ huynh chưa đóng học phí");
 				System.out.println("5. Top 3 phụ huynh có số lượng đăng kí lớp cho trẻ học nhiều nhất");
+				System.out.println("6. Hiển thị Phụ huynh có từ 2 con trở lên học chung trường");
 				System.out.println();
-				System.out.println("6. Thoat khoi chuong trinh");
+				System.out.println("7. Thoat khoi chuong trinh");
 				System.out.println();
 				System.out.println("Nhap lua chon cua ban: ");
 				String choice = input.nextLine();
 				switch (choice) {
-					case "1": 
+				case "1":
 					PhuHuynh x = new PhuHuynh();
 					x.inputInfo();
 					PHdao.insertPH(x);
-						break;
-					case "2":
+					break;
+				case "2":
 					PhuHuynhDao x1 = new PhuHuynhDao();
 					x1.updatePhuHuynh();
-						break;
-					case "3":
-						PhuHuynhDao x2 = new PhuHuynhDao();
-						x2.deleteIDPhuHuynh();
-						break;
-					//case "4":
-						//entities.EmployeeDao.deleteEmployee();
-						//break;
-					//case "5":
-						//entities.EmployeeDao.updatetuoi();
-						//break;
-					case "6":
-						return;
-					default:
-						System.out.println("thong tin ban nhap chua dung, xin hay kiem tra lai");
-						break;
+					break;
+				case "3":
+					PhuHuynhDao x2 = new PhuHuynhDao();
+					x2.deleteIDPhuHuynh();
+					break;
+				 case "4":
+					PhuHuynhDao x3 = new PhuHuynhDao();
+					x3.displayHocPhiCanDong();
+				 break;
+				case "5":
+					PhuHuynhDao x4 = new PhuHuynhDao();
+					x4.displayTop3();
+					break;
+				case "6":
+					PhuHuynhDao x5 = new PhuHuynhDao();
+					x5.displayPH();
+					break;
+				case "7":
+					return;
+				default:
+					System.out.println("thong tin ban nhap chua dung, xin hay kiem tra lai");
+					break;
 				}
 			} catch (Exception e) {
 				// TODO: handle exception
